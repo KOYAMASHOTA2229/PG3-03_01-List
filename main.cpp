ac7b354bedc0b29b67956d61f6bd7cd60eb81388
KoyamaShota <k023g1039@g.neec.ac.jp>
2024-05-23 16:19:41 +0900

    03_01 確認課題 (動作確認済み)

--- main.cpp
+++ main.cpp
@@ -9,15 +9,43 @@ int main() {
 		,"Mejiro","Takadanobaba","Shin-Okubo","Sinjuku","Yoyogi","Harajuku","Shibuya","Ebisu" ,"Meguro"
 		,"Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho","Simbashi","Yurakucho" };
 
+	//1970年の駅名
 	printf("1970年\n");
 
-	//1970年の駅名
+	//リストを順番に読み込み
 	for (auto itr = stationList.begin(); itr != stationList.end(); ++itr) {
 		std::cout << *itr << "\n";
 	}
 
 	//2019年の駅名
-	printf("")
+	printf("\n2019年\n");
+
+	//新たな駅名をリストの適切な位置に挿入
+	for (std::list<const char*>::iterator itr = stationList.begin(); itr != stationList.end(); ++itr) {
+		if (*itr == "Tabata") {
+			itr = stationList.insert(itr, "Nishi - Nippori");
+			++itr;
+		}
+	}
+	//リストを順番に読み込み
+	for (auto itr = stationList.begin(); itr != stationList.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+
+	//2022年の駅名
+	printf("\n2022年\n");
+
+	//新たな駅名をリストの適切な位置に挿入
+	for (std::list<const char*>::iterator itr = stationList.begin(); itr != stationList.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			itr = stationList.insert(itr, "Takanawa Gateway");
+			++itr;
+		}
+	}
+	//リストを順番に読み込み
+	for (auto itr = stationList.begin(); itr != stationList.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
 
 	return 0;
 }
